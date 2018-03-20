b8e0c7de64037664abb57801851aa6278d960cc3
Jery <jery@MacBook-Pro-cmadmin-2.local>
2018-03-20 11:41:59 +0200

    dish

--- do.php
+++ do.php
@@ -7,6 +7,7 @@ if (mysqli_connect_errno()) {
     exit();
 }
 mysqli_select_db($link,'meal');
+mysqli_query($link, 'DELETE FROM `dish`');
 mysqli_query($link,"SET NAMES 'utf8'");
 
 $mon=$_POST['mon'];
@@ -24,14 +25,20 @@ $mon=$_POST['mon'];
         $i=$i+1;
         }
         
-/*
+
 $tue=$_POST['tue'];
         $tue_1 = explode("\n",$tue[1]);
         $tue_2 = explode("\n",$tue[2]);
         $tue_3 = explode("\n",$tue[3]);
         $tue_4 = explode("\n",$tue[4]);
         $tue_5 = explode("\n",$tue[5]);
-        echo="$tue_1, $tue_2, $tue_3, $tue_4, $tue_5 <br>"
+        for($i=1;$i<=5; ){
+            foreach(${'tue_'.$i} as $value){
+                $query = 'INSERT INTO dish (name, day, meal) VALUES ("'.$value.'","2","'.$i.'")';
+                mysqli_query($link, $query);
+            }
+            $i=$i+1;
+            }
 
 $wen=$_POST['wen'];
         $wen_1 = explode("\n",$wen[1]);
@@ -39,7 +46,13 @@ $wen=$_POST['wen'];
         $wen_3 = explode("\n",$wen[3]);
         $wen_4 = explode("\n",$wen[4]);
         $wen_5 = explode("\n",$wen[5]);
-        echo="$wen_1, $wen_2, $wen_3, $wen_4, $wen_5 <br>"
+        for($i=1;$i<=5; ){
+            foreach(${'wen_'.$i} as $value){
+                $query = 'INSERT INTO dish (name, day, meal) VALUES ("'.$value.'","3","'.$i.'")';
+                mysqli_query($link, $query);
+            }
+            $i=$i+1;
+            }
 
 $thu=$_POST['thu'];
         $thu_1 = explode("\n",$thu[1]);
@@ -47,7 +60,13 @@ $thu=$_POST['thu'];
         $thu_3 = explode("\n",$thu[3]);
         $thu_4 = explode("\n",$thu[4]);
         $thu_5 = explode("\n",$thu[5]);
-        echo="$thu_1, $thu_2, $thu_3, $thu_4, $thu_5 <br>"
+        for($i=1;$i<=5; ){
+            foreach(${'thu_'.$i} as $value){
+                $query = 'INSERT INTO dish (name, day, meal) VALUES ("'.$value.'","4","'.$i.'")';
+                mysqli_query($link, $query);
+            }
+            $i=$i+1;
+            }
 
 $fri=$_POST['fri'];
         $fri_1 = explode("\n",$fri[1]);
@@ -55,17 +74,14 @@ $fri=$_POST['fri'];
         $fri_3 = explode("\n",$fri[3]);
         $fri_4 = explode("\n",$fri[4]);
         $fri_5 = explode("\n",$fri[5]);
-        echo="$fri_1, $fri_2, $fri_3, $fri_4, $fri_5 <br>"
-
-       /* $title = $value["title"];
-        $price = $value["price"];
-        $desc = $value["description"];
-        echo "$image, $title, $price, $desc<br>";
-        $query = 'INSERT INTO test (image, title, price, description) VALUES ("'.$image.'","'.$title.'","'.$price.'","'.$desc.'")';
-        echo "$query <br>";
-        mysqli_query($link, $query);*/
-
-
-
+        for($i=1;$i<=5; ){
+            foreach(${'fri_'.$i} as $value){
+                $query = 'INSERT INTO dish (name, day, meal) VALUES ("'.$value.'","5","'.$i.'")';
+                mysqli_query($link, $query);
+            }
+            $i=$i+1;
+            }
 mysqli_close($link);
+
+echo "Данные сохранены!";
 ?>
