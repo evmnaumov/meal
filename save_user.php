ead7db20f91a16e344495a7802ffa6c4f25d6dc4
Jery <jery@MacBook-Pro-cmadmin-2.local>
2018-03-20 14:56:05 +0200

    klajh

--- save_user.php
+++ save_user.php
@@ -24,7 +24,7 @@ include ("bd.php");
 
 // проверка на существование пользователя с таким же логином
 $result = mysqli_query($link, "SELECT id FROM users WHERE login='$login'");
-$myrow = mysqli_fetch_array($result);
+$myrow = mysqli_fetch_assoc($result);
 if (!empty($myrow['id'])) {
 exit ("Извините, введённый вами логин уже зарегистрирован. Введите другой логин.");
 }
