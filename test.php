483059c2ccca7e01c6f487dd9a446498268f93a8
Евгений Наумов <jery@JeryMac.local>
2018-04-12 11:48:10 +0300

    asdf

--- test.php
+++ test.php
@@ -6,10 +6,10 @@ $answer = mysqli_query($link,$answer_query);
 while ($ans_row = mysqli_fetch_assoc($answer)){
 $ans_row = $ans_row[''.$qday.''];
 $ameal = explode(",",$ans_row);
-echo $ameal[0];
-echo $ameal[1];
-echo $ameal[2];
-echo $ameal[3];
+echo $ameal[0]."<br>";
+echo $ameal[1]."<br>";
+echo $ameal[2]."<br>";
+echo $ameal[3]."<br>";
 echo $ameal[4];
 }
 ?>
