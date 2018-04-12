b7b503da49321859e90c9e273d2a56c2cb680ee8
Евгений Наумов <jery@JeryMac.local>
2018-04-12 11:27:03 +0300

    test

--- test.php
+++ test.php
@@ -3,7 +3,7 @@ include("bd.php");
 $qday = "mon";
 $answer_query = "SELECT `'.$qday.'` FROM `answers` WHERE user_id=`4`";
 $answer = mysqli_query($link,$answer_query);
-while ($ans_row = mysqli_fetch_assoc($answer)){
-print_r($ans_row);
-}
+//while ($ans_row = mysqli_fetch_assoc($answer)){
+print_r($answer);
+//}
 ?>
