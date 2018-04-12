44298742a99cc5d512bd8d6b3142fa8948881a0c
Евгений Наумов <jery@JeryMac.local>
2018-04-12 13:18:50 +0300

    asdf

--- testreg.php
+++ testreg.php
@@ -19,7 +19,7 @@ $password = stripslashes($password);
     include ("bd.php");// файл bd.php должен быть в той же папке, что и все остальные, если это не так, то просто измените путь 
  
 $result = mysqli_query($link,'SELECT * FROM users WHERE login=`'.login.'`'); //извлекаем из базы все данные о пользователе с введенным логином
-    $user = mysqli_fetch_assoc($result);
+    $user = mysqli_fetch_array($result, MYSQLI_NUM);
     if (empty($user['login']))
     {
     //если пользователя с введенным логином не существует
