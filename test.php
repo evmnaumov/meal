7dc7b66baa79982688a0f9328b4bcca9c5db738e
Евгений Наумов <jery@JeryMac.local>
2018-04-12 11:39:48 +0300

    asdf

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
 include("bd.php");
 $qday = "mon";
-$answer_query = "SELECT '.$qday.', tue FROM `answers` WHERE user_id=`4`";
+$answer_query = 'SELECT `'.$qday.'`, `tue` FROM `answers` WHERE user_id="4"';
 $answer = mysqli_query($link,$answer_query);
 while ($ans_row = mysqli_fetch_assoc($answer)){
 print_r($ans_row);
