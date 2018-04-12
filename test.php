62707f47fa1807fb0504088bdb0431f4b068d345
Евгений Наумов <jery@JeryMac.local>
2018-04-12 14:56:46 +0300

    test

--- test.php
+++ test.php
@@ -1,10 +1,12 @@
 <?php
 include("bd.php");
-$dish_query='SELECT dish_id, name FROM `dish` WHERE day="1" AND meal="1"';
+for($i=1;$i<=5; $i++){
+$dish_query='SELECT dish_id, name FROM `dish` WHERE day="'.$i.'" AND meal="1"';
 $dish= mysqli_query($link,$dish_query);
 while ($dish_arr = mysqli_fetch_assoc($dish)){
 echo $dish_arr['dish_id'];
 echo $dish_arr['name'];
 echo "<br>";
+}
 }
 ?>
