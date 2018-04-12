b38d574dbc18bfaf83036a2d905f5db80398cb91
Евгений Наумов <jery@JeryMac.local>
2018-04-12 18:49:43 +0300

    asdf

--- lc.php
+++ lc.php
@@ -69,10 +69,10 @@ for($j=1;$j<=5;){
         $dish_query=mysqli_query($link,'SELECT `dish_id`, `name` FROM `dish` WHERE day="'.$i.'" AND meal="'.$j.'"');
         //$dish= $dish_query);
         while ($dish_arr = mysqli_fetch_assoc($dish_query)){
-                print_r($dish_arr);
+                $dish[] = $dish_arr[0];
                 echo "<td class=\"col_$j\">
                         <select id=\"list\" name=\"answer[$i][$j]\" form=\"answers\">";
-                foreach($dish_arr as $dish_id=>$dish_name){
+                foreach($dish as $dish_id=>$dish_name){
                 //проверяем, выбрана ли позиция
                 /*if ($dish_id = $ameal[''.$a.'']){
                     $sel = "selected";
