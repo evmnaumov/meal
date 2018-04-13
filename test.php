3e48363e6a05258910144da7e45e7bf662676f13
Евгений Наумов <jery@JeryMac.local>
2018-04-13 17:11:41 +0300

    asdf

--- test.php
+++ test.php
@@ -2,8 +2,8 @@
 include("bd.php");
 $answer_query = 'SELECT `mon` FROM `answers` WHERE user_id="4"';
 $answer = mysqli_query($link,$answer_query);
-while ($ans_row = mysqli_fetch_assoc($answer)){
-        $ans = $ans_row[''.$qday.''];
+while ($ans_row = mysqli_fetch_row($answer)){
+        $ans = $ans_row[0];
         $ameal = explode(",",$ans);
         print_r($ameal);
 }
