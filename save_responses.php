5f0abbfcc3e45efc14bf4fe8e790f42e992ae680
Евгений Наумов <jery@JeryMac.local>
2018-04-13 18:28:43 +0300

    save

--- save_responses.php
+++ save_responses.php
@@ -8,21 +8,34 @@ include ("bd.php");
     mysqli_query($link, 'DELETE FROM `answers` WHERE user_id="'.$user_id.'"');
 }
 mysqli_query($link,"SET NAMES 'utf8'");
-
+*/
 $day=$_POST['answer'];
 $i=1;
 foreach($day as $value){
-    $j=1;
-    foreach($value as $dish){
-        $data = explode("\n",$dish);
-        foreach($data as $text){
-            $query = 'INSERT INTO dish (name, day, meal) VALUES ("'.$text.'","'.$i.'","'.$j.'")';
-            mysqli_query($link, $query);
-        }
-        $j=$j+1;
-        }
-    $i=$i+1;
-    }*/
+   switch($i){
+       case 1:
+        $mon = implode(",", $value);
+        echo $mon;
+        break;
+       case 2:
+        $tue = implode(",", $value);
+        echo $tue;
+        break;
+       case 3:
+        $wen = implode(",", $value);
+        echo $wen;
+        break; 
+        case 4:
+        $thu = implode(",", $value);
+        echo $thu;
+        break;
+        case 5:
+        $fri = implode(",", $value);
+        echo $fri;
+        break;
+   }
+$i=$i+1;
+}
 echo $login;
 print_r($_POST);
 mysqli_close($link);
