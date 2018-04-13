44a593208c7ffe09ec78aa057880356b5dc09423
Евгений Наумов <jery@JeryMac.local>
2018-04-13 17:06:34 +0300

    asdf

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 include("bd.php");
-$answer_query = 'SELECT `'.$qday.'` FROM `answers` WHERE user_id="'.$user_id.'"';
+$answer_query = 'SELECT `mon` FROM `answers` WHERE user_id="4"';
 $answer = mysqli_query($link,$answer_query);
 while ($ans_row = mysqli_fetch_assoc($answer)){
         $ans = $ans_row[''.$qday.''];
