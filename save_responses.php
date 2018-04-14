35a507ddab2775382ea9d5e204684a34ff3eed7d
Евгений Наумов <jery@JeryMac.local>
2018-04-14 12:50:18 +0300

    asdf

--- save_responses.php
+++ save_responses.php
@@ -7,7 +7,7 @@ $admin = $_SESSION['admin'];
 include ("bd.php");
 
 //проверяем, существует ли запись ответов пользователя, если нет - создаем
-$user_ans = msqli_query($link, 'SELECT * FROM asnwers WHERE user_id = "'.$user_id.'"');
+$user_ans = mysqli_query($link, 'SELECT * FROM asnwers WHERE user_id = "'.$user_id.'"');
 $user_exist = mysqli_fetch_row($user_ans);
 print_r($user_exist);
 if(emty($user_exist[0])){
--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 include ("bd.php");
 
 //проверяем, существует ли запись ответов пользователя, если нет - создаем
-$user_ans = msqli_query($link, 'SELECT * FROM asnwers WHERE user_id = "'.$user_id.'"');
+$user_ans = mysqli_query($link, 'SELECT * FROM asnwers WHERE user_id = "'.$user_id.'"');
 while($user_exist = mysqli_fetch_row($user_ans)){
 print_r($user_exist);
 }
