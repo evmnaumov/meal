757e3b25026f2acb01b05f907afd748c71a4d915
Евгений Наумов <jery@JeryMac.local>
2018-04-14 14:53:24 +0300

    asdf

--- admin.php
+++ admin.php
@@ -1,3 +1,21 @@
 <?php
 include ("session.php");
+//если пользователь не админ - нехуй шастать=)
+if($admin==0){
+    header("Location: http://".$_SERVER['HTTP_HOST']."/meal/lc.php");
+}
+if (empty($_GET['page'])){
+    $page = "menu.php";
+}
 ?>
+<html>
+<head>
+<title>Админ-панель</title>
+</head>
+<body>
+<p><?php echo $user_name." ".$user_last_name; ?></p>
+<p>Вы вошли с логином: <?php echo $login; ?></p>
+<p>Вернуться в <a href="lc.php">личный кабинет</a><p>
+<iframe src="<?php echo $page; ?>" frameborder="0"></iframe>
+</body>
+</html>
