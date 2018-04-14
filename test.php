6004d36acf8d895549939fa2e905c91f33afb590
Евгений Наумов <jery@JeryMac.local>
2018-04-14 12:51:39 +0300

    asdf

--- test.php
+++ test.php
@@ -2,9 +2,9 @@
 include ("bd.php");
 
 //проверяем, существует ли запись ответов пользователя, если нет - создаем
-$user_ans = mysqli_query($link, 'SELECT * FROM asnwers WHERE user_id = "'.$user_id.'"');
+$user_ans = mysqli_query($link, 'SELECT * FROM asnwers WHERE user_id = "3"');
 while($user_exist = mysqli_fetch_row($user_ans)){
 print_r($user_exist);
 }
-mysqli_free_result($user_ansasdf);
+mysqli_free_result($user_ans);
 ?>
