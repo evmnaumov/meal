a5b08140e15e26c87a6a297aae4f8966a2a727da
Евгений Наумов <jery@JeryMac.local>
2018-04-19 13:25:46 +0300

    asdf

--- responses.php
+++ responses.php
@@ -48,6 +48,7 @@ while($loc_row = mysqli_fetch_row($location)){
             }
             $answer = mysqli_query($link, 'SELECT `'.$day.'` FROM `answers` WHERE user_id="'.$user_row[0].'"');
             while ($answer_row = mysqli_fetch_row($answer)){
+                print_r($answer_row);
             if(empty($answer_row)){
                 for($d=1;$d<=25;){
                     echo "<td>-----</td>";
